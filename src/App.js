5a50d6ebc1393d816c01bcb153a529f300ad83db
unknown <aditya3011.1990@gmail.com>
2023-05-22 15:36:09 +0530

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
-
+import { Route,Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import ProductDetail from './pages/productDetails/ProductDetail';
+import Categories from './pages/categories/Categories';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className="App">
-    Hare Krishna
+      <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/products' element={<Categories/>}/>
+      <Route path='/products/:id' element={<ProductDetail/>}/>
+    </Routes>
+    <Footer/>
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import './navbar.scss'
+import { Link } from 'react-router-dom'
+import {BsCart2} from 'react-icons/bs'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='NavBar'>
+      <div className='container nav-container'>
+      <div className='nav-left'>
+        <ul className='link-group'>
+          <li className='hover'>
+            <Link className='link' to='/products?categories=comic'>Comics</Link>
+          </li>
+          <li className='hover'>
+          <Link className='link'to='/products?categories=shows'>TV Shows</Link>
+          </li>
+          <li className='hover'>
+          <Link className='link' to='/products?categories=sports'>Sports</Link>
+          </li>
+        </ul>
+      </div>
+      <div className='nav-center'>
+      <Link to='/'><h1 className='banner'>PosterZ.</h1></Link>
+      </div>
+      <div className='nav-right'>
+        <BsCart2 className='icon'/>
+        <span className='cart-count'>9</span>
+      </div>
+      </div>
+    </div>
   )
 }
 
